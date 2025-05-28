81ad7158076a2faf877a36e20ffdda523d069adc
gitcodingrank <jiraja2915@gmail.com>
2025-05-28 10:16:35 +0530

    Done for creating war

--- app1_schoolmanagementsystem/src/main/java/com/school/App1SchoolmanagementsystemApplication.java
+++ app1_schoolmanagementsystem/src/main/java/com/school/App1SchoolmanagementsystemApplication.java
@@ -2,10 +2,17 @@ package com.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App1SchoolmanagementsystemApplication {
-
+public class App1SchoolmanagementsystemApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(App1SchoolmanagementsystemApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App1SchoolmanagementsystemApplication.class, args);
 	}
